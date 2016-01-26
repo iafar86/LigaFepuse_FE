f145697bee885428017b94d9baf402bf4599e919
franpaz <franfpaz@gmail.com>
2016-01-26 16:54:43 -0300

    cambios en el routing, agregue funcionalidad para el detalle del torneo y alta de fechas y partidos

--- App/ligaFepuseApp.js
+++ App/ligaFepuseApp.js
@@ -1,4 +1,5 @@
-﻿var ligaFepuseApp = angular.module('ligaFepuseApp', ['ngMaterial', 'ng-mfb','ngMdIcons', 'ngResource', 'ui.router', 'ngCookies', 'ngTable', 'ngSanitize', 'ngAnimate',
+﻿
+var ligaFepuseApp = angular.module('ligaFepuseApp', ['ngMaterial', 'ng-mfb','ngMdIcons', 'ngResource', 'ui.router', 'ngCookies', 'ngTable', 'ngSanitize', 'ngAnimate',
  'ngAria', 'ct.ui.router.extras', 'angular-loading-bar', 'daypilot', 'LocalStorageModule', 'angular-jwt', 'ui.bootstrap'])
     .config(function ($stateProvider, $urlRouterProvider, $httpProvider, $stickyStateProvider, cfpLoadingBarProvider) {
         //'ngResource', 'ngMdIcons', 'ui.router', 'ngCookies', 'ngTable',
@@ -7,40 +8,57 @@
 
         cfpLoadingBarProvider.includeSpinner = true;
         cfpLoadingBarProvider.includeBar = true;
+        
 
-        //$urlRouterProvider.when('', '/App/Dashboard/Home');
-
-        $urlRouterProvider.otherwise("/App/Dashboard/Home");
+        $urlRouterProvider.otherwise("/App/Home");
 
         $stateProvider
-        .state('app', {
-            abstract: true,
-            url: '/App',            
-            templateUrl: 'index.html'
 
+        //#region App
+        .state('app', {            
+            abstract: true,	
+            url: '/App',
+            views: {
+                '': {
+                    templateUrl: 'App/Template/layout.html'
+                },
+                'content': {
+                    templateUrl: 'App/Dashboard/Dashboard.html'
+                }
+            }
         })
 
-        .state('app.dashboard', {
-            url: '/Dashboard',
-            templateUrl: 'App/Dashboard/Dashboard.html'
-        })
-
-        .state('app.dashboard.home', {
+        .state('app.home', {
             url: '/Home',
             templateUrl: 'App/Template/home.html'
         })
 
-        .state('app.dashboard.ui', {
+        .state('app.ui', {
             url: '/UI',
             templateUrl: 'App/Template/ui.html'
         })
 
-        .state('app.dashboard.table', {
+        .state('app.table', {
             url: '/Table',
             templateUrl: 'App/Template/table.html'
         })
 
-        .state('app.dashboard.torneo', {
+         //#endregion
+
+        //#region Torneos
+            .state('torneo', {
+                abstract: true,
+                url: '',
+                views: {
+                    '': {
+                        templateUrl: 'App/Template/layout.html'
+                    },
+                    'content': {
+                        templateUrl: 'App/Dashboard/Dashboard.html'
+                    }
+                }
+            })
+        .state('torneo.list', {
             url: '/Torneos',
             templateUrl: 'App/Torneo/Partials/torneoAdd.html',
             controller: 'torneoCtrl',
@@ -48,16 +66,66 @@
                 torneoDataFactory: 'torneoDataFactory',
                 torneoList: function (torneoDataFactory) {
                     return torneoDataFactory.getTorneos();
+                },
+                infoTorneo: function (torneoDataFactory) {
+                    return { value: [] };
                 }
             }
         })
+             .state('torneo.info', {
+                 url: '/Torneo/:torneoId',
+                 templateUrl: 'App/Torneo/Partials/torneoInfo.html',                 
+                 controller: 'torneoCtrl',
+                 resolve: {
+                     torneoDataFactory: 'torneoDataFactory',
+                     torneoList: function () {
+                         return { value: [] };
+                     },
+                     infoTorneo: function (torneoDataFactory, $stateParams) {
+                         var torneoId = $stateParams.torneoId;
+                         return torneoDataFactory.getTorneo(torneoId);
+                     }
+                 }
+             })
+            .state('torneo.info.fecha', {
+                url: '/Fecha/:fechaId',
+                views: {
+                    'partidos': {
+                        templateUrl: 'App/Partido/Partials/partidosList.html',
+                        controller: 'fechaCtrl',
+                        resolve: {
+                            fechaDataFactory: 'fechaDataFactory',
+                            listPartidos: function (fechaDataFactory, $stateParams) {
+                                var fechaId = $stateParams.fechaId;
+                                return fechaDataFactory.getFecha(fechaId);
+                                //return fechaDataFactory.get({ id: fechaId });
+                            }                                                        
+                        }
+                    }
+                }
+            })
+
+            //#endregion
 
          .state('app.dashboard.login', {
              url: '/Login',
              templateUrl: 'App/Template/login.html'
          })
 
-         .state('app.dashboard.equipos', {
+        //#region Equipos
+            .state('equipo', {
+                abstract: true,
+                url: '',
+                views: {
+                    '': {
+                        templateUrl: 'App/Template/layout.html'
+                    },
+                    'content': {
+                        templateUrl: 'App/Dashboard/Dashboard.html'
+                    }
+                }
+            })
+         .state('equipo.equipos', {
              url: '/Equipos',
              templateUrl: 'App/Equipo/Partials/equipoAdd.html',
              controller: 'equipoCtrl',
@@ -75,6 +143,7 @@
                  //}                
              }
          })
+        //#endregion
 
 
 
